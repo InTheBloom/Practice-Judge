321773bf91b9fcc3357c831b70c5b56e253f4bc3
InTheBloom <nato.rider.smm2@gmail.com>
2026-03-13 13:39:08 +0900

    [fix] katexのimportを修正

--- front/app/routes/problem_editorial.tsx
+++ front/app/routes/problem_editorial.tsx
@@ -1,15 +1,10 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router";
 import { BASEURL } from '../backend_url';
+import "katex/dist/katex.min.css";
 import parse from 'html-react-parser';
 import renderMathInElement from '../auto-render';
 import { AceEditorReadOnly } from '../ace_editor';
-
-export function links () {
-    return [
-        { rel: "stylesheet", href: new URL("/static/katex/katex.min.css", BASEURL).href },
-    ];
-}
 
 export function meta({ data }: Route.MetaArgs) {
     let title = "問題が見つかりません";
@@ -55,7 +50,7 @@ export default function Editorial ({ params, loaderData }) {
     // DOMパース -> <code editor language="text"></code>部分をaceeditorに置換
     const parser = new DOMParser();
     const problemDOM = parser.parseFromString(problem.editorial, "text/html");
-    renderMathInElement(problemDOM);
+    renderMathInElement(problemDOM.body);
 
     const editorialJSX = parse(problemDOM.body.innerHTML, {
         replace (domNode) {
@@ -91,69 +86,5 @@ export default function Editorial ({ params, loaderData }) {
                 {editorialJSX.length == 0 ? "この問題には解説がありません。" : editorialJSX}
             </article>
         </main>
-    );
-}
-
-function SampleCopyButton ({ children, onClick, className }) {
-    const [copied, setCopied] = useState(false);
-    // useEffectはcopiedCodeの変化で発火し、trueへの変化ならwaitしてfalseにセットする。
-    useEffect(() => {
-        if (!copied) {
-            return;
-        }
-
-        const id = setTimeout(() => setCopied(false), 1000);
-        return () => clearTimeout(id);
-    }, [copied]);
-
-    const props = {
-        onClick: (e) => {
-            onClick(e);
-            setCopied(true);
-        },
-        className: className,
-    };
-    if (copied) {
-        props["data-tooltip"] = "コピーしました！";
-    }
-
-    return (
-        <button
-            {...props}
-        >
-            {children}
-        </button>
-    );
-}
-
-function AIhelpButton ({ children, onClick }) {
-    const [copied, setCopied] = useState(false);
-    // useEffectはcopiedCodeの変化で発火し、trueへの変化ならwaitしてfalseにセットする。
-    useEffect(() => {
-        if (!copied) {
-            return;
-        }
-
-        const id = setTimeout(() => setCopied(false), 1000);
-        return () => clearTimeout(id);
-    }, [copied]);
-
-    const props = {
-        onClick: () => {
-            onClick();
-            setCopied(true);
-        },
-    };
-    if (copied) {
-        props["data-tooltip"] = "コピーしました！";
-    }
-
-    return (
-        <button
-            {...props}
-            className="outline"
-        >
-            {children}
-        </button>
     );
 }
